9b1e457b0aac284b4c1eb8cf39fd7760e69a5e4a
Andrew McGovern <winedark.mcgovern@gmail.com>
2017-12-25 16:08:55 -0600

    first commit

--- PokeFinder/AppDelegate.swift
+++ PokeFinder/AppDelegate.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 import CoreData
+import FirebaseDatabase
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Andrew McGovern on 12/8/17.
+//  Copyright © 2017 Andrew McGovern. All rights reserved.
+//
+
+
+#import "GeoFire.h"
